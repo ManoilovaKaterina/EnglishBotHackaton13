edab0d02feda07530ed96df7b626c107a20ac980
ManoilovaKaterina <katya.manoylova@gmail.com>
2024-09-06 17:33:11 +0300

    Add project files.

---
+++ EnglishBotHackaton/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Program
+{
+    private static TelegramBotClient Client;
+    private static ConcurrentDictionary<long, string> _userFileRequests = new ConcurrentDictionary<long, string>();
+    private static CancellationTokenSource _cts = new CancellationTokenSource();
+
+    static async Task Main(string[] args)
+    {
+        Client = new TelegramBotClient("7313187643:AAFw5dBBBRMn1O6McVDVTRRViWX76I-yI80", cancellationToken: _cts.Token);
+        var me = await Client.GetMeAsync();
+
+        Console.WriteLine($"@{me.Username} is running...");
+
+        await SetBotCommandsAsync();
+
+        Client.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            },
+            cancellationToken: _cts.Token
+        );
+
+        Console.ReadLine();
+        _cts.Cancel();
+    }
+
+    private static async Task SetBotCommandsAsync()
+    {
+        var commands = new List<BotCommand>
+        {
+            new BotCommand { Command = "start", Description = "Старт" }
+        };
+
+        await Client.SetMyCommandsAsync(commands);
+        Console.WriteLine("Команди виставлені");
+    }
+
+    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Type == UpdateType.Message && update.Message is { } message)
+        {
+            await OnMessage(message);
+        }
+    }
+
+    private static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(exception);
+    }
+
+    private static async Task OnMessage(Message msg)
+    {
+        if (msg.Text == "/start")
+        {
+            await Client.SendTextMessageAsync(msg.Chat.Id, "Привіт! Я - твій бот для навчання англійської");
+        }
+    }
+}
