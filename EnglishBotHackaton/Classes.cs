83db77d348d9976d9a7b94783608353e02132de5
ManoilovaKaterina <katya.manoylova@gmail.com>
2024-09-07 12:59:05 +0300

    dict

--- EnglishBotHackaton/Classes.cs
+++ EnglishBotHackaton/Classes.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace EnglishBotHackaton
 {
@@ -21,4 +22,30 @@ namespace EnglishBotHackaton
         }
     }
 
+    public static class WordListProvider
+    {
+        public static List<WordEntry> WordList { get; private set; } = LoadWordList();
+
+        private static List<WordEntry> LoadWordList()
+        {
+            string filePath = "dictionary.txt";
+
+            var wordList = new List<WordEntry>();
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    string word = parts[0].Trim();
+                    string translation = parts[1].Trim();
+                    string definition = parts[2].Trim();
+
+                    wordList.Add(new WordEntry(word, translation, definition));
+                }
+            }
+
+            return wordList;
+        }
+    }
 }
